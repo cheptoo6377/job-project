b6749f044796d015a6c735fa6c0ecd5bc763320c
cheptoo6377 <cheptoodorothy69@gmail.com>
2025-07-22 13:09:59 +0300

    changes

--- config.py
+++ config.py
@@ -6,17 +6,13 @@ load_dotenv()
 
 
 class Config:
-    if os.getenv('DATABASE_URL'):
-        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    else:
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///api.db'
+    # Use DATABASE_URL for production, fallback to SQLite for local development
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///api.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY','my_strong_secret')
     BASE_API_URL = os.getenv('API_URL')  
     SECURITY_PASSWORD_SALT = os.getenv("SECURITY_PASSWORD_SALT", "default_salt")
     SECURITY_PASSWORD_HASH = os.getenv("SECURITY_PASSWORD_HASH", "bcrypt")
-                                         
-        
 
     SECURITY_REGISTERABLE = True
     SECURITY_SEND_REGISTER_EMAIL = False
@@ -27,8 +23,3 @@ class Config:
     SECURITY_AUTO_LOGIN_AFTER_REGISTER = True
     SECURITY_LOGIN_WITHOUT_CONFIRMATION = True
     SECURITY_CONFIRMABLE = False
-    POSTGRES_USER = os.getenv('POSTGRES_USER', 'dorothy')
-    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', '6377')
-    POSTGRES_DB = os.getenv('POSTGRES_DB', 'jobdb')
-    POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
-    POSTGRES_PORT = os.getenv('POSTGRES_PORT', '5432')  
